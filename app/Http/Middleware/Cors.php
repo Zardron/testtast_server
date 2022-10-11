9f22a9ff1ac98f8d2925e3a72a57e5702a0edfe2
Zardron <zardron@clockworkdelivery.com>
2022-10-11 15:49:30 +0400

    fix cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Origin: https://testtask2022.herokuapp.com/');
+        header('Access-Control-Allow-Origin: https://testtask2022.herokuapp.com');
         header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods: POST, PUT');
         return $next($request);
